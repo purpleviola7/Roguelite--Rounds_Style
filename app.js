25656e9f92becf10f2310e656c12a2a9a1042e27
purpleviola7 <cbaumann13@protonmail.com>
2026-08-16 12:23:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 let text = "Hello";
 alert(text);
-document.body.style.backgroundColor = "lightGreen"
+document.body.style.backgroundColor = "green"
+const button = document.getElementById('submitBtn');
+let input = document.getElementById('userInput');
+let display = document.getElementById('display');
+button.addEventListener('click', () => {
+let number = input.value
+if(number = 0){
+display.textContent = "Your number is-- Zero")
+}
+else if(number > 0){
+  if(number % 2 = 0){
+display.textContent = "Your number is-- Positive, Even"
+  }
+  else{
+    display.textContent = "Your number is-- Positive, Odd"
+  }
+}
+else if(number < 0){
+  if(number % 2 = 0{
+    display.textContent = "Your number is-- Negative, Even"
+  }
+else{
+  display.textContent = "Your number is-- Negative, Odd"
+}
+}
